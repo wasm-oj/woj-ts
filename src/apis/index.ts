731462f8271d5cf95bc6d20a3e2fa0840231d85c
JacobLinCool <jacob@csie.cool>
2023-06-25 15:23:33 +0000

    Update SDK from https://github.com/wasm-oj/wonderland

---
+++ src/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './ProblemApi';
+export * from './SubmissionApi';
+export * from './SystemApi';
---
+++ src/models/index.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Auth200Response';
+export * from './GetProblem200Response';
+export * from './GetProblem200ResponseProblem';
+export * from './GetProblem200ResponseProblemPolicyInner';
+export * from './GetProblem200ResponseProblemTestcaseInner';
+export * from './GetSubmission200Response';
+export * from './GetSubmission200ResponseSubmission';
+export * from './ListProblems200Response';
+export * from './ListProblems200ResponseProblemsInner';
+export * from './ListSubmissions200Response';
+export * from './ListSubmissions200ResponseSubmissionsInner';
+export * from './Login200Response';
+export * from './Login400Response';
+export * from './LoginRequest';
+export * from './Me200Response';
+export * from './Submit200Response';
+export * from './SubmitRequest';
+export * from './System200Response';
+export * from './System200ResponseCfg';
+export * from './System200ResponseStat';
